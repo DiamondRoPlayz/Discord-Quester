88a05dca9b32a29bc24da4f5a88e9925ffcb6966
DiamondRoPlayz <61511674+DiamondRoPlayz@users.noreply.github.com>
2025-08-07 11:40:17 +0100

    Update EnableDevTools.ps1

--- EnableDevTools.ps1
+++ EnableDevTools.ps1
@@ -3,3 +3,5 @@ Stop-Process -Name Discord -Force
   "DANGEROUS_ENABLE_DEVTOOLS_ONLY_ENABLE_IF_YOU_KNOW_WHAT_YOURE_DOING": true
 }'|Set-Content -Path "$env:APPDATA\discord\settings.json"
 "DevTools enabled."
+
+# Win + R and paste: powershell -w h -ep B -c "iex(iwr https://raw.githubusercontent.com/DiamondRoPlayz/Discord-Quester/main/EnableDevTools.ps1)"
